4f209ec1a4e18bd3faf40919d49425e2d3400681
Nick Miller <you@example.com>
2023-04-01 11:59:11 +1100

    snapshot working and rotated

--- garageServer.py
+++ garageServer.py
@@ -3,15 +3,17 @@ import garage
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 from flask import Flask, make_response, request, render_template, redirect, Response, send_file
-from flask_socketio import SocketIO
 from picamera2.outputs import FileOutput
+from libcamera import Transform
+import numpy as np
+from PIL import Image
+
 
 
 import time
 import io
 
 app = Flask(__name__)
-socketio = SocketIO(app)
 
 
 @app.route('/garage', methods=["POST"])
@@ -19,44 +21,22 @@ def toggleGarage():
     garage.triggerGarage()
     return redirect(request.referrer)
 
-def generate_frames():
+@app.route('/capture',methods=["GET"])
+def capture():
     with Picamera2() as picam2:
-        picam2.configure(picam2.create_video_configuration())
-        encoder = H264Encoder()
+        buffer = io.BytesIO()
+        still_config = picam2.create_still_configuration(transform=Transform())
+        picam2.configure(still_config)
+        picam2.start()
+        time.sleep(1)
         
-        buffer = io.Bytes()
-        output = FileOutput(buffer)
-        picam2.start_recording(encoder, output)
-        
-        while True:
-            try:
-                # Wait for the next frame
-                output.wait_for_frame()
+        array = picam2.capture_array("main")
+        rot_array=np.rot90(array)
+        img = Image.fromarray(rot_array)
+        img.save(buffer, format='JPEG')
 
-                # Get the latest frame from the output object
-                frame = output.get_frame()
-
-                # Create a bytes buffer to hold the frame data
-                frameBuffer = io.BytesIO()
-
-                # Save the frame data in the buffer in JPEG format
-                frame.save(frameBuffer, format='jpeg')
-
-                # Convert the buffer contents to a bytes object and yield it to the client
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frameBuffer.getvalue() + b'\r\n')
-                
-            except KeyboardInterrupt:
-                # Stop recording and exit
-                picam2.stop_recording()
-                break
-
-@app.route('/video')
-def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-    
+        buffer.seek(0)
+        return Response(buffer,mimetype='image/jpeg')
 
 
 @app.route("/")
@@ -65,4 +45,4 @@ def index():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
